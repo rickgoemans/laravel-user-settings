71b9ae17008bb14ab66f2c06c535192fcf29fcac
Rick Goemans <rickgoemans@gmail.com>
2023-05-22 13:19:08 +0200

    Fixed enum import

--- database/factories/UserSettingFactory.php
+++ database/factories/UserSettingFactory.php
@@ -4,7 +4,7 @@ namespace Rickgoemans\LaravelUserSettings\Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Database\Eloquent\Model;
-use OutSmart\Core\Domain\Settings\Enums\UserSettingType;
+use Rickgoemans\LaravelUserSettings\Enums\UserSettingType;
 use Rickgoemans\LaravelUserSettings\Models\UserSetting;
 
 /** @extends Factory<UserSetting> */
@@ -17,15 +17,15 @@ class UserSettingFactory extends Factory
     {
         return [
             'group' => fake()->word,
-            'key' => fake()->word,
-            'type' => fake()->randomElement(UserSettingType::cases()),
+            'key'   => fake()->word,
+            'type'  => fake()->randomElement(UserSettingType::cases()),
             'value' => null,
         ];
     }
 
     public function forUser(Model $user): self
     {
-        return $this->state(function (array $attributes) use ($user): array {
+        return $this->state(function(array $attributes) use ($user): array {
             return [
                 'user_id' => $user->getKey(),
             ];
