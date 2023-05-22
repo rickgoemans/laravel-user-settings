e3c4b79b543ef2b4e15cf8937bab0d363293470c
Rick Goemans <rickgoemans@gmail.com>
2023-05-22 15:49:11 +0200

    Added middleware

---
+++ src/Middleware/MigrateUserSettings.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Rickgoemans\LaravelUserSettings\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Rickgoemans\LaravelUserSettings\Services\UserSettingService;
+use Symfony\Component\HttpFoundation\Response;
+
+class MigrateUserSettings
+{
+    /** @param Closure(Request): (Response) $next */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (Auth::user()) {
+            /** @var UserSettingService $service */
+            $service = app(UserSettingService::class);
+
+            $service->migrate();
+        }
+
+        return $next($request);
+    }
+}
