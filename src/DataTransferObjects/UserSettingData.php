968846e100687e13452f237141c129016779a4d7
Rick Goemans <rickgoemans@gmail.com>
2023-06-09 11:33:55 +0200

    Corrected import

--- src/DataTransferObjects/UserSettingData.php
+++ src/DataTransferObjects/UserSettingData.php
@@ -2,6 +2,7 @@
 
 namespace Rickgoemans\LaravelUserSettings\DataTransferObjects;
 
+use Carbon\Carbon;
 use Rickgoemans\LaravelUserSettings\Enums\UserSettingType;
 use Spatie\LaravelData\Data;
 
@@ -9,13 +10,14 @@ class UserSettingData extends Data
 {
     public function __construct(
         public UserSettingType $type,
-        public string $group,
-        public string $key,
-        public mixed $value = null,
-        public ?int $user_id = null,
-        public ?Carbon $created_at = null,
-        public ?Carbon $updated_at = null,
-    ) {
+        public string          $group,
+        public string          $key,
+        public mixed           $value = null,
+        public ?int            $user_id = null,
+        public ?Carbon         $created_at = null,
+        public ?Carbon         $updated_at = null,
+    )
+    {
 
     }
 }
